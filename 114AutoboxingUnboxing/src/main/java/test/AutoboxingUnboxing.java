9b8df57d2b40f04e4616dc4e0c40e389f14a48df
NAME1 <juanfelipemurielvalencia@gmail.com>
2020-07-11 15:23:59 -0500

    Ejercicio aplicando Autoboxing y Unboxing

--- 114AutoboxingUnboxing/src/main/java/test/AutoboxingUnboxing.java
+++ 114AutoboxingUnboxing/src/main/java/test/AutoboxingUnboxing.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package test;
 
 /**
- *
+ * 
  * @author juanfelipemurielvalencia
  */
 public class AutoboxingUnboxing {
-    
-}
+    public static void main(String[] args) {
+         //autboxing (envolvemos tipos primitivos en clases wrapper)
+         Integer enteroObj = 10;
+         System.out.println("enteroObj : "  + enteroObj);// impresion normal
+         
+         //Unboxing (extraemos tipo primitivo del objeto envolvente)
+         int entero = enteroObj;
+         System.out.println("entero: " + entero);
+         
+         Float floatObj = 15.4f; //Autoboxing
+         System.out.println("floatObj : " + floatObj);
+         float flotante = floatObj; //Unboxing
+         System.out.println("flotante " + flotante);
+         
+         //Solicitar un tipo int a un objetivo de la clase Float (object)
+         System.out.println("int a partir de un Float: "  + floatObj.intValue() );
+         
+    }
+}  
+ 
